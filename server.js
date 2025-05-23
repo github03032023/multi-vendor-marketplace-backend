e2226851817bedb9234cfd5dd535938d1287c92d
Dhanya S <dhanyanair20222@gmail.com>
2025-05-23 11:29:26 +0400

    Cors Issue Rectified

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
+
 const connectDb = require("./config/db");
 
 const fs = require('fs');
@@ -11,52 +11,63 @@ if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir);
 }
 
-
 dotenv.config();
-const app= express();
+
+const cors = require("cors");
+const app = express();
+
+const corsOptions = {
+  origin: 'https://multi-vendor-marketplace-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  // credentials: true, // Only if you use cookies
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 //  Make 'uploads' folder publicly accessible
 app.use('/uploads', express.static(uploadsDir));
 
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 connectDb();
 
 // Customer Routes
-const customerRoutes= require("./routes/customerRoutes");
+const customerRoutes = require("./routes/customerRoutes");
 app.use("/api/auth/customer", customerRoutes);
 
 // Vendor Routes
-const vendorRoutes= require("./routes/vendorRoutes");
+const vendorRoutes = require("./routes/vendorRoutes");
 app.use("/api/auth/vendor", vendorRoutes);
 
 // Admin Routes
-const adminRoutes= require("./routes/adminRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 app.use("/api/auth/admin", adminRoutes);
 
 
 // AdminDashboard  Routes
-const adminDashboardRoutes= require("./routes/adminDashboardRoutes");
+const adminDashboardRoutes = require("./routes/adminDashboardRoutes");
 app.use("/api/auth/adminDashboard", adminDashboardRoutes);
 
 // Product Routes
-const productRoutes= require("./routes/productRoutes");
+const productRoutes = require("./routes/productRoutes");
 app.use("/api/auth/product", productRoutes);
 
 // Cart Routes
-const cartRoutes= require("./routes/cartRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 app.use("/api/auth/cart", cartRoutes);
 
 // Order Routes
-const orderRoutes= require("./routes/orderRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 app.use("/api/auth/order", orderRoutes);
 
 
 // Payment Routes
-const paymentRoutes= require("./routes/paymentRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
 app.use("/api/auth/payment", paymentRoutes);
 
 
@@ -81,24 +92,24 @@ app.use("/api/auth/notifications", notificationRoutes);
 
 
 // Product Review Routes
-const productReviewRoutes= require("./routes/productReviewRoutes");
+const productReviewRoutes = require("./routes/productReviewRoutes");
 app.use("/api/auth/productReview", productReviewRoutes);
 
 const analyticsRoutes = require("./routes/analyticsRoutes");
 app.use("/api/auth/analytics", analyticsRoutes);
 
 // Payout Routes
-const payoutRoutes= require("./routes/payoutRoutes");
+const payoutRoutes = require("./routes/payoutRoutes");
 app.use("/api/auth/payout", payoutRoutes);
 
 
 // Platform Commission Routes
-const commisssionRoutes= require("./routes/adminCommissionFormRoutes");
+const commisssionRoutes = require("./routes/adminCommissionFormRoutes");
 app.use("/api/auth/commission", commisssionRoutes);
 
-app.get("/", (req,res)=>{
-    res.send("Multi Vendor Market Place is running");
+app.get("/", (req, res) => {
+  res.send("Multi Vendor Market Place is running");
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
